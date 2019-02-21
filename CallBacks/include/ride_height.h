20c855318d8dd6c3b761ab1d29e5329db806477c
Bijan Tabatabai <bijan311@yahoo.com>
2019-02-20 18:16:00 -0600

    Added ride height code
    Signed-off-by: Bijan Tabatabai <bijan311@yahoo.com>

---
+++ CallBacks/include/ride_height.h
@@ -0,0 +1,13 @@
+/*
+ * ride_height.h
+ *
+ *  Created on: Feb 18, 2019
+ *      Author: bijan
+ */
+
+#ifndef CALLBACKS_INCLUDE_RIDE_HEIGHT_H_
+#define CALLBACKS_INCLUDE_RIDE_HEIGHT_H_
+
+int init_ride_height(void);
+
+#endif /* CALLBACKS_INCLUDE_RIDE_HEIGHT_H_ */
