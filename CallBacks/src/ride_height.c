1360d2836280954b2509e86164ae40c6381e7da9
Bijan Tabatabai <bijan311@yahoo.com>
2019-04-03 18:55:46 -0500

    Changes the ride height to be powered by roboclaws
    Signed-off-by: Bijan Tabatabai <bijan311@yahoo.com>

--- CallBacks/src/ride_height.c
+++ CallBacks/src/ride_height.c
@@ -7,23 +7,24 @@
 #include "FreeRTOS.h"
 #include "hardware_abstraction_layer.h"
 #include "MechanicalControlPackets.h"
-#include "ess.h"
+#include "roboclaw_driver.h"
 #include "task.h"
 #include "bcl_port.h"
 
-#define RIDE_HEIGHT_I2C_ADDR (0x0A)
-#define FRONT_PAIR_SHIFT              (0x02)
-#define BACK_PAIR_SHIFT             (0x00)
-#define STOP                 (0x00)
-#define DRIVE_UP        (0x01)
-#define DRIVE_DOWN          (0x02)
-
 //The I2C bus that the ride height board is on
-eI2CController rhBus = i2c_invalid;
 static BCL_STATUS ride_height_callback(int bcl_inst, BclPayloadPtr payload);
+
+static motor_controller front_rh;
+static motor_controller back_rh;
 
 int init_ride_height(void)
 {
+    // initialize each joint driver
+    if(roboclaw_driver_init(&front_rh, uart0, 0x83, 0, 0, true))// last arg = motor 1
+            return 1;
+    if(roboclaw_driver_init(&back_rh, uart0, 0x83, 0, 0, false))// last arg = motor 2
+            return 1;
+
     BCL_pktCallbackRegister(ride_height_callback, SET_RIDE_HEIGHT_SPEED);
 
     return 0;
@@ -32,24 +33,22 @@ int init_ride_height(void)
 BCL_STATUS ride_height_callback(int bcl_inst, BclPayloadPtr payload)
 {
     RideHeightPayload *pyld = (RideHeightPayload *)payload;
-    uint8_t data;
 
-    if(rhBus == i2c_invalid)
-        rhBus = ESS_findDevice(RIDE_HEIGHT_I2C_ADDR);
+    if(pyld->front > 0) {
+        front_rh.set_speed(&front_rh, 64);
+    } else if(pyld->front < 0) {
+        front_rh.set_speed(&front_rh, -64);
+    } else {
+        front_rh.set_speed(&front_rh, 0);
+    }
 
-    //Tell the ride height board what to do!
-    data = 0;
-    //Move both the front and the back forward
-    if(pyld->front > 0) data |= DRIVE_UP << FRONT_PAIR_SHIFT;
-    else if(pyld->front < 0) data |= DRIVE_DOWN << FRONT_PAIR_SHIFT;
-    else data |= STOP << FRONT_PAIR_SHIFT;
-
-    if(pyld->back > 0) data |= DRIVE_UP << BACK_PAIR_SHIFT;
-    else if(pyld->back < 0) data |= DRIVE_DOWN << BACK_PAIR_SHIFT;
-    else data |= STOP << BACK_PAIR_SHIFT;
-
-    //Send the stuff
-    ESS_sendCommand(rhBus, RIDE_HEIGHT_I2C_ADDR, ESS_CMD_RIDE_HEIGHT, &data, 1);
+    if(pyld->back > 0) {
+        back_rh.set_speed(&back_rh, 64);
+    } else if(pyld->back < 0) {
+        back_rh.set_speed(&back_rh, -64);
+    } else {
+        back_rh.set_speed(&back_rh, 0);
+    }
 
     return BCL_OK;
 }
